a465b1bada9820736eb1e6eafcb52ebe86aa87b8
Issam <issam.boudrar@fendora.nl>
2025-07-10 14:28:28 +0200

    move core repo one level up

--- main.tf
+++ main.tf
@@ -8,6 +8,6 @@ module "feedback" {
 
 module "lambda_core" {
   source             = "./modules/lambda-core"
-  lambda_package_path = "core/dist/lambda.zip"
+  lambda_package_path = "../core/dist/lambda.zip"
   openai_api_key     = var.openai_api_key
 }
